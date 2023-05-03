a5ec4954bc1ab80ffcfa0148c2f3f71e07f0e331
Jared Martinez <jaredartt@gmail.com>
2023-05-03 19:50:40 +0200

    All routes put before the port

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000.');
-});
-
 app.get('/name', (req, res) => {
   res.send('My name is Jared Martinez.');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000.');
 });
